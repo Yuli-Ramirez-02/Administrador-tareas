27e90df87c530ca3b4cc7f96ae0f483635011b00
Yuli Ramireze <ramirezyuli412@gmail.com>
2026-07-11 18:35:54 -0500

    Fixing errors

--- src/main/java/gm/tareas/controlador/IndexControlador.java
+++ src/main/java/gm/tareas/controlador/IndexControlador.java
@@ -1,6 +1,10 @@
 package gm.tareas.controlador;
 
+import gm.tareas.servicio.TareaServicio;
 import javafx.fxml.Initializable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
@@ -8,6 +12,11 @@ import java.util.ResourceBundle;
 
 @Component
 public class IndexControlador implements Initializable {
+    private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
+
+    @Autowired
+    private TareaServicio tareaServicio;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
